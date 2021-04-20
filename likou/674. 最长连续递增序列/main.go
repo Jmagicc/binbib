35870cbaf5dab1ffbcee6e8b6db0f30797bd2cee
Katell11 <FYZ20190211>
2021-04-20 21:19:05 +0800

    尝试提交

--- likou/674. 最长连续递增序列/main.go
+++ likou/674. 最长连续递增序列/main.go
@@ -6,7 +6,6 @@ func main() {
 	fmt.Println(findLengthOfLCIS([]int{1, 3, 5, 4, 7})) // 3
 	fmt.Println(findLengthOfLCIS([]int{2, 2, 2, 2, 2})) // 1
 
-	println(lpp(2))
 }
 func findLengthOfLCIS(nums []int) int {
 	n := len(nums)
@@ -16,27 +15,17 @@ func findLengthOfLCIS(nums []int) int {
 	maxcount := 0
 	slow := 0
 	for slow < n-1 {
-		count := 1
 		fast := slow
+		count := 1
 		for fast < n-1 && nums[fast] < nums[fast+1] {
-			count++
 			fast++
+			count++
 		}
 		slow++
 		if count > maxcount {
 			maxcount = count
 		}
-
 	}
+
 	return maxcount
-}
-
-func lpp(li int) int {
-	count := 0
-	for ok := true; ok; ok = li < 15 {
-		count++
-		li++
-	}
-
-	return count
 }
